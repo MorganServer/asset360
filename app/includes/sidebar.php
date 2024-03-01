0f1777fbff2edb881718c69047e8266e7f0211fb
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 08:22:52 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,6 +1,6 @@
 <div class="sidebar">
     
-<ul class="list-group list-group-flush">
+<ul class="list-group list-group-flush bg-dark">
     <li class="list-group-item"><a href="">Dashboard</a></li>
     <li class="list-group-item"><a href="">Server & PC</a></li>
     <li class="list-group-item"><a href="">Devices</a></li>
