87299fd272ae6f27b193a8caad562e06c9424fb9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 10:31:24 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -5,13 +5,12 @@
         <li class="list-group-item"><a href=""><i class="bi bi-pc-display"></i> Server & PC</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-printer-fill"></i> Devices</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-keyboard"></i> Accessories</a></li>
-        <li class="list-group-item"><a href=""><i class="bi bi-geo-fill"></i> IP Address</a></li>
-        <li class="list-group-item"><a href=""><i class="bi bi-key-fill"></i> Access</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-geo-fill"></i> IP Addresses</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-database-fill"></i> Databases</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-building-fill-gear"></i> System Setting</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-gear-wide-connected"></i> Manage Utility</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-people-fill"></i> Manage Users</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-file-earmark-arrow-down-fill"></i> Bulk Import</a></li>
-        <li class="list-group-item"><a href=""><i class="bi bi-database-fill"></i> Database Backup</a></li>
     </ul>
 
 </div>
--- index.php
+++ index.php
@@ -28,39 +28,119 @@ session_start();
 
             <div class="mt-4"></div>
 
-            <div class="card-container">
-                <div class="card" style="">
-                    <div class="card-body text-center">
-                        <p class="card-text">
-                            <div class="card_text_left float-start" style="font-size: 45px;">
-                                <i class="bi bi-hdd-stack ps-4"></i>
-                            </div>
-                            <div class="card_text_right float-end pe-3">
-                                <p class="text-end">9</p>
-                                <p class="text-muted text-end">Servers</p>
-                            </div>
-                        </p>
+            <!-- Top Card Container -->
+                <div class="card-container">
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-hdd-stack ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">9</p>
+                                    <p class="text-muted text-end">Servers</p>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-pc-display-horizontal ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">1</p>
+                                    <p class="text-muted text-end">Desktops</p>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-laptop ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">6</p>
+                                    <p class="text-muted text-end">Laptops</p>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-tablet ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">9</p>
+                                    <p class="text-muted text-end">Tablets</p>
+                                </div>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Location</h5>
-                        <p class="card-text"><?php //echo $location; ?></p>
+            <!-- end Top Card Container -->
+
+            <!-- Bottom Card Container -->
+                <div class="card-container">
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-printer ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">2</p>
+                                    <p class="text-muted text-end">Devices</p>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-keyboard ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">4</p>
+                                    <p class="text-muted text-end">Accessories</p>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-geo-fill ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">6</p>
+                                    <p class="text-muted text-end">IP Addresses</p>
+                                </div>
+                            </p>
+                        </div>
+                    </div>
+                    <div class="card" style="">
+                        <div class="card-body text-center">
+                            <p class="card-text">
+                                <div class="card_text_left float-start" style="font-size: 45px;">
+                                    <i class="bi bi-database-fill ps-4"></i>
+                                </div>
+                                <div class="card_text_right float-end pe-3">
+                                    <p class="text-end">3</p>
+                                    <p class="text-muted text-end">Databases</p>
+                                </div>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Job Type</h5>
-                        <p class="card-text"><?php //echo $job_type; ?></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <div class="card-body text-center">
-                        <h5 class="card-title">Job Type</h5>
-                        <p class="card-text"><?php //echo $job_type; ?></p>
-                    </div>
-                </div>
-            </div>
+            <!-- end Bottom Card Container -->
 
         </div>
 
