7ba9d0cc56844d6de90e39a7134d33b48948e9e3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 07:38:28 -0600

    update header 1.1

--- app/includes/header.php
+++ app/includes/header.php
@@ -3,9 +3,9 @@
     <a class="navbar-brand" href="#">
         <img src="<?php echo BASE_URL . "/assets/images/logo.png"; ?>" class="header_logo" alt="Logo">
     </a>
-    <form class="d-flex" role="search">
+    <div class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
+    </div>
   </div>
 </nav>
