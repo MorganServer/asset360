f75f04718456de55c0e157f8a6ed152635fe2449
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 07:25:25 -0600

    debugg

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,7 +1,7 @@
 <nav class="navbar">
   <div class="container-fluid">
     <a class="navbar-brand"><img src="<?php echo ROOT_PATH . '/assets/images/logo.png'; ?>" alt=""></a>
-    <?php echo ROOT_PATH; ?>
+    <?php echo ROOT_PATH . '/assets/images/logo.png' ?>
     <form class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
