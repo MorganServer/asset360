ede12d81b1685e4bb7fb07a547f02a9486b85738
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 09:48:19 -0600

    update

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ session_start();
 
     <!-- Main Content -->
 
-        <div class="container" style="background-color: blue;">
+        <div class="container" style="background-color: blue;margin-left: 200px;">
 
             <div class="mt-4"></div>
 
