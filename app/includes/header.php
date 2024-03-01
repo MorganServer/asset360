3cd9112f7b082a058062eebd518bf6e1228100c9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 15:22:24 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -3,16 +3,12 @@
     <a class="navbar-brand" href="#">
         <img src="<?php echo BASE_URL . "/assets/images/logo.png"; ?>" class="header_logo" alt="Logo">
     </a>
-    <div class="header_right d-flex dropdown">
-        <div class="dropdown">
-          <i class="bi bi-person-circle" id="profileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-          <div class="dropdown-menu" aria-labelledby="profileDropdown">
-            <a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> Profile</a>
-            <a class="dropdown-item" href="index.php?logout=1"><i class="bi bi-box-arrow-right"></i> Logout</a>
-          </div>
-        </div>
+    <div class="dropdown d-flex dropdown">
+      <i class="bi bi-person-circle" id="profileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+      <div class="dropdown-menu" aria-labelledby="profileDropdown">
+        <a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> Profile</a>
+        <a class="dropdown-item" href="index.php?logout=1"><i class="bi bi-box-arrow-right"></i> Logout</a>
       </div>
-            
     </div>
   </div>
 </nav>
