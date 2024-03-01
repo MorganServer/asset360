0780569ef0a0d12de7ab6f101bb0ebc9a2cae53d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 07:38:48 -0600

    update header 1.2

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,8 +4,7 @@
         <img src="<?php echo BASE_URL . "/assets/images/logo.png"; ?>" class="header_logo" alt="Logo">
     </a>
     <div class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
+        <i class="bi bi-person-circle"></i>
     </div>
   </div>
 </nav>
