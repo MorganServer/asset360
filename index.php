4fbec554e2d713e1800b65ee8be2607b9c6b56bc
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 10:24:47 -0600

    update

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ session_start();
                 <div class="card pt-3 pb-3" style="">
                     <div class="card-body text-center">
                         <p class="card-text">
-                            <div class="card_text_left float-start" style="font-size: 45px;">
+                            <div class="card_text_left float-start" style="font-size: 55px;">
                                 <i class="bi bi-hdd-stack ps-4"></i>
                             </div>
                             <div class="card_text_right float-end pe-3">
