9733e517a83ec1fb366317a33d1305c038f10881
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 08:13:46 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -6,10 +6,8 @@
     <div class="header_right d-flex dropdown">
             <i class="bi bi-person-circle" id="profileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
             <div class="dropdown-menu" aria-labelledby="profileDropdown">
-                <a class="dropdown-item" href="#">Profile</a>
-                <a class="dropdown-item" href="#">Settings</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Logout</a>
+                <a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> Profile</a>
+                <a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a>
             </div>
     </div>
   </div>
