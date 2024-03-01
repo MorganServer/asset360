4656e5567913b09f493dc4dbf804b54e8d01762d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 08:43:04 -0600

    update: add icons to sidebar

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,10 +1,10 @@
 <div class="sidebar">
     
     <ul class="list-group list-group-flush">
-        <li class="list-group-item"><a href="">Dashboard</a></li>
-        <li class="list-group-item"><a href="">Server & PC</a></li>
-        <li class="list-group-item"><a href="">Devices</a></li>
-        <li class="list-group-item"><a href="">Accessories</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-house-fill"></i> Dashboard</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-pc-display"></i> Server & PC</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-printer-fill"></i>Devices</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-keyboard"></i> Accessories</a></li>
     </ul>
 
 </div>
