c36ca7393e1275cbbb763fc81594363221886998
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 08:27:15 -0600

    update all

---
+++ app/database/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = 'localhost';
+$user = 'dbuser';
+$pass = 'DBuser123!';
+$db = 'assets';
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if(!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
+} else {
+    // echo 'Successful connection!';
+}
+
+?>
---
+++ app/includes/sidebar.php
@@ -0,0 +1,10 @@
+<div class="sidebar">
+    
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item"><a href="">Dashboard</a></li>
+        <li class="list-group-item"><a href="">Server & PC</a></li>
+        <li class="list-group-item"><a href="">Devices</a></li>
+        <li class="list-group-item"><a href="">Accessories</a></li>
+    </ul>
+
+</div>
