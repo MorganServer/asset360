294d9a1fbf58bf65b4f8cd909cb6d52de73a0faf
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 14:39:07 -0600

    update

--- login.php
+++ login.php
@@ -32,14 +32,14 @@ if(isLoggedIn() == true) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
   <!-- Custom CSS -->
-  <link rel="stylesheet" href="assets/css/loginpage.css?v=<?php echo time(); ?>">
+  <link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time(); ?>">
 
   <!-- Page Title -->
   <title>Sign In | WMS</title>
 
 </head>
 
-<body>
+<body class="login-body">
     <div class="main" style="height: 450px;">
         <p class="sign">Sign in</p>
         <p class="sub_sign">Work Management System</p>
