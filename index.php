eeca9133b3f984765895f63ad542dc7d7ca4b33c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 14:40:25 -0600

    updating functions

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+date_default_timezone_set('America/Denver');
+require_once "app/database/connection.php";
 require_once "path.php";
 session_start();
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$files = glob("app/functions/*.php");
+foreach ($files as $file) {
+    require_once $file;
+}
+
+loginUser($conn);
+if(isLoggedIn() == true) {
+    header('location:' . BASE_URL . '/');
+}
 ?>
 
 <!DOCTYPE html>
