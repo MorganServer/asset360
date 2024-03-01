2d2c78b4f9652f8986aede9b7c8d13a3be117b87
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 06:53:43 -0600

    comment out success message in connection

--- app/database/connection.php
+++ app/database/connection.php
@@ -10,7 +10,7 @@ $conn = mysqli_connect($host, $user, $pass, $db);
 if(!$conn) {
     die("Connection Failed: " . mysqli_connect_error());
 } else {
-    echo 'Successful connection!';
+    // echo 'Successful connection!';
 }
 
 ?>
