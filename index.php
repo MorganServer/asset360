d6b1754fcbdb2f0a029187cb6e854f3a8c5b6888
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 07:29:40 -0600

    adding css

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ session_start();
     <!-- Bootstrap Links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <!-- Custom Styles -->
+    <link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time(); ?>">
+
     <title>Testing</title>
 </head>
 <body>
