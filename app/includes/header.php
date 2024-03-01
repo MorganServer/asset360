bb7e706c79af89a88a44e629558d5594ef8aa74f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 07:43:26 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -4,7 +4,15 @@
         <img src="<?php echo BASE_URL . "/assets/images/logo.png"; ?>" class="header_logo" alt="Logo">
     </a>
     <div class="header_right d-flex">
-        <i class="bi bi-person-circle"></i>
+    <div class="dropdown">
+        <i class="bi bi-person-circle dropdown-toggle" id="profileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
+        <div class="dropdown-menu" aria-labelledby="profileDropdown">
+            <a class="dropdown-item" href="#">Profile</a>
+            <a class="dropdown-item" href="#">Settings</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Logout</a>
+        </div>
     </div>
+</div>
   </div>
 </nav>
