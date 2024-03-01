4417ba1d22f802aa2775a72d561c45f3b70c70ba
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 07:39:06 -0600

    update header 1.3

--- app/includes/header.php
+++ app/includes/header.php
@@ -3,7 +3,7 @@
     <a class="navbar-brand" href="#">
         <img src="<?php echo BASE_URL . "/assets/images/logo.png"; ?>" class="header_logo" alt="Logo">
     </a>
-    <div class="d-flex" role="search">
+    <div class="d-flex">
         <i class="bi bi-person-circle"></i>
     </div>
   </div>
