814cb5eade1c52869ef45e3b482c1e049ec3bcba
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 17:19:34 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
     
     <ul class="list-group list-group-flush">
-        <li class="list-group-item"><a href=""><i class="bi bi-house-fill"></i> Dashboard</a></li>
+        <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/"><i class="bi bi-house-fill"></i> Dashboard</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-pc-display"></i> Server & PC</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-printer-fill"></i> Devices</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-keyboard"></i> Accessories</a></li>
