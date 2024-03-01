36c149dfb4c9994d92820558c588352e4f1a7ad9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 09:37:59 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -3,8 +3,15 @@
     <ul class="list-group list-group-flush">
         <li class="list-group-item"><a href=""><i class="bi bi-house-fill"></i> Dashboard</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-pc-display"></i> Server & PC</a></li>
-        <li class="list-group-item"><a href=""><i class="bi bi-printer-fill"></i>Devices</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-printer-fill"></i> Devices</a></li>
         <li class="list-group-item"><a href=""><i class="bi bi-keyboard"></i> Accessories</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-geo-fill"></i> IP Address</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-key-fill"></i> Access</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-building-fill-gear"></i> System Setting</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-gear-wide-connected"></i> Manage Utility</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-people-fill"></i> Manage Users</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-file-earmark-arrow-down-fill"></i> Bulk Import</a></li>
+        <li class="list-group-item"><a href=""><i class="bi bi-database-fill"></i> Database Backup</a></li>
     </ul>
 
 </div>
