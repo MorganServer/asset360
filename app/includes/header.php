858ddccb64cbe02c9e9dbf9ecc1d23fdc4a0c665
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 07:23:16 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -1,6 +1,6 @@
-<nav class="navbar bg-body-tertiary">
+<nav class="navbar">
   <div class="container-fluid">
-    <a class="navbar-brand">Navbar</a>
+    <a class="navbar-brand"><img src="<?php echo ROOT_PATH; ?>/assets/images/logo.png" alt=""></a>
     <form class="d-flex" role="search">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success" type="submit">Search</button>
