69fae548baab73e83a8cba10ee387c0f4e5a4520
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 18:41:12 -0600

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -8,6 +8,7 @@
         <i class="bi bi-plus-circle-fill" id="profileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
         <div class="dropdown-menu" aria-labelledby="profileDropdown">
           <a class="dropdown-item" href="<?php echo BASE_URL; ?>/add-asset.php"><i class="bi bi-pc-display"></i> Add Asset</a>
+          <a class="dropdown-item" href="<?php echo BASE_URL; ?>/add-ip.php"><i class="bi bi-geo-fill"></i> Add IP Address</a>
           <!-- <a class="dropdown-item" href="index.php?logout=1"><i class="bi bi-box-arrow-right"></i> Logout</a> -->
         </div>
       </div>
