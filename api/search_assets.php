fd694c135fae02fec51514526be82775f91d426d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 22:31:06 -0600

    update

--- api/search_assets.php
+++ api/search_assets.php
@@ -12,12 +12,11 @@ if (isset($_POST['keyword'])) {
     // Display dropdown menu with matching assets
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
+            // Output each asset as a link with onclick event to selectAsset function
             echo '<a href="#" class="list-group-item list-group-item-action asset-link" onclick="selectAsset(' . $row['asset_id'] . ', \'' . $row['asset_name'] . '\')">' . $row['asset_name'] . '</a>';
-
         }
     } else {
         echo 'No assets found';
     }
 }
 ?>
-
