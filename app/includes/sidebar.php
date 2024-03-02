210535f4b78b14b20cb318dd062c65f97b7f0ce9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 18:13:02 -0600

    update

--- app/includes/sidebar.php
+++ app/includes/sidebar.php
@@ -4,9 +4,9 @@
         <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/"><i class="bi bi-house-fill"></i> Dashboard</a></li>
         <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/servers.php"><i class="bi bi-hdd-stack"></i> Servers</a></li>
         <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/computers.php"><i class="bi bi-pc-display-horizontal"></i> Computers</a></li>
-        <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/network-devices.php"><i class="bi bi-diagram-2"></i> Network Devices</a></li>
-        <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/mobile-devices.php"><i class="bi bi-phone"></i> Mobile Devices</a></li>
-        <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/storage.php"><i class="bi bi-device-ssd"></i> Storage</a></li>
+        <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/network.php"><i class="bi bi-diagram-2"></i> Network Devices</a></li>
+        <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/mobile.php"><i class="bi bi-phone"></i> Mobile Devices</a></li>
+        <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/storage.php"><i class="bi bi-device-ssd"></i> Storage Devices</a></li>
         <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/ip-address.php"><i class="bi bi-geo-fill"></i> IP Addresses</a></li>
         <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/iot.php"><i class="bi bi-tv"></i> IOT Devices</a></li>
         <li class="list-group-item"><a href="<?php echo BASE_URL; ?>/pages/peripherals.php"><i class="bi bi-printer"></i> Peripherals</a></li>
