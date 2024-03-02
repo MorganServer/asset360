6a5df462cdccfa69712e16801be85db29ce6658e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 18:01:53 -0600

    update

--- pages/servers.php
+++ pages/servers.php
@@ -84,7 +84,7 @@ if(isLoggedIn() == false) {
                     <th scope="row"><?php echo $asset_tag_no; ?></th>
                     <td><?php echo $asset_name ? $asset_name : '-'; ?></td>
                     <td><?php echo $location ? $location : '-'; ?></td>
-                    <td><?php echo $f_maintenance_schedule ? $maintenance_schedule : '-'; ?></td>
+                    <td><?php echo $f_maintenance_schedule ? $f_maintenance_schedule : '-'; ?></td>
                     <td><?php //echo $f_audit_schedule ? $audit_schedule : '-'; ?></td>
                     <td><?php echo $status ? $status : '-'; ?></td>
                     <td style="font-size: 20px;"><a href="view-app.php?viewid=<?php echo $id; ?>" class="view"><i class="bi bi-eye text-success"></i></a> &nbsp; <a href="update-app.php?updateid=<?php echo $id; ?>"><i class="bi bi-pencil-square" style="color:#005382;"></a></i> &nbsp; <a href="open-app.php?deleteid=<?php echo $id; ?>" class="delete"><i class="bi bi-trash" style="color:#941515;"></i></a></td>
