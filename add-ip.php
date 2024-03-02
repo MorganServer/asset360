c530d174635f921ad23e63e62b4a32a60c28aed3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-01 22:13:46 -0600

    update

--- add-ip.php
+++ add-ip.php
@@ -125,7 +125,7 @@ if(isLoggedIn() == false) {
 
 <!-- Modal -->
 <div class="modal fade" id="assetModal" tabindex="-1" aria-labelledby="assetModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-scrollable">
+    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="assetModalLabel">Select Asset</h5>
