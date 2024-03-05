df07f6ca94ef2efd880645949646e47de2cb607a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-05 08:23:31 -0600

    update

--- app/functions/convert-pdf.php
+++ app/functions/convert-pdf.php
@@ -1,8 +1,17 @@
 <?php
 
-include_once(ROOT_PATH . '/TCPDF/tcpdf.php');
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+
+include_once(ROOT_PATH . '/TCPDF/tcpdf.php');
+
+$pdf = new TCPDF('P', 'mm', 'A4');
+
+$pdf->AddPage();
+
+$pdf->Output();
+
+
 
 ?>
