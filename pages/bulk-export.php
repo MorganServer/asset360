3032c619e9019e703ad0bbd59a4d9fa62a555d1b
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-05 18:41:01 -0600

    update

--- pages/bulk-export.php
+++ pages/bulk-export.php
@@ -176,6 +176,7 @@ if($num_rows > 0) {
         </div>
     <!-- END main-container -->
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
 $(document).ready(function() {
