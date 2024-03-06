9b07eb1cc0e7b4195e7b93de468fe69ae02ecf79
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-03-05 18:02:34 -0600

    update

--- pages/convert-pdf.php
+++ pages/convert-pdf.php
@@ -33,6 +33,8 @@ $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
 // Set header logo
 $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 001', PDF_HEADER_STRING);
 $pdf->setFooterData(array(0,64,0), array(0,64,128));
+$pdf->setHeaderTitleAlign('C');
+$pdf->setHeaderStringAlign('C');
 
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
