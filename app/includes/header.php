a166e4f2db5c35ce171df7f975f7033410fcbca2
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-04-28 23:00:51 -0500

    update

--- app/includes/header.php
+++ app/includes/header.php
@@ -16,7 +16,7 @@
         <i class="bi bi-person-circle" id="profileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
         <div class="dropdown-menu" aria-labelledby="profileDropdown">
           <a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> Profile</a>
-          <a class="dropdown-item" href="index.php?logout=1"><i class="bi bi-box-arrow-right"></i> Logout</a>
+          <a class="dropdown-item" href="<?php echo BASE_URL . "/?logout=1" ?>"><i class="bi bi-box-arrow-right"></i> Logout</a>
         </div>
       </div>
     </div>
