6cd1074eede654cf8cc0c6de5a1932ace6099530
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-04-29 11:59:38 -0500

    ASSET-1 1.31 - building the modal for review

--- pages/event-log.php
+++ pages/event-log.php
@@ -577,31 +577,31 @@ if(isLoggedIn() == false) {
 </div>
 
 <script>
-    document.addEventListener('DOMContentLoaded', function() {
-    var accordionButton = document.getElementById('accordion-button');
-    if (accordionButton) {
-        var chev_i = document.getElementById('chev');
+//     document.addEventListener('DOMContentLoaded', function() {
+//     var accordionButton = document.getElementById('accordion-button');
+//     if (accordionButton) {
+//         var chev_i = document.getElementById('chev');
         
-        if (chev_i) {
-            accordionButton.addEventListener('click', function() {
+//         if (chev_i) {
+//             accordionButton.addEventListener('click', function() {
                 
-                var isCollapsed = accordionButton.classList.contains('collapsed');
+//                 var isCollapsed = accordionButton.classList.contains('collapsed');
                 
-                if (isCollapsed) {
-                    chev_i.classList.remove('bi-chevron-up');
-                    chev_i.classList.add('bi-chevron-down');
-                } else {
-                    chev_i.classList.remove('bi-chevron-down');
-                    chev_i.classList.add('bi-chevron-up');
-                }
-            });
-        } else {
-            console.log('Chevron icon not found');
-        }
-    } else {
-        console.log('Accordion button not found');
-    }
-});
+//                 if (isCollapsed) {
+//                     chev_i.classList.remove('bi-chevron-up');
+//                     chev_i.classList.add('bi-chevron-down');
+//                 } else {
+//                     chev_i.classList.remove('bi-chevron-down');
+//                     chev_i.classList.add('bi-chevron-up');
+//                 }
+//             });
+//         } else {
+//             console.log('Chevron icon not found');
+//         }
+//     } else {
+//         console.log('Accordion button not found');
+//     }
+// });
 
 </script>
 
