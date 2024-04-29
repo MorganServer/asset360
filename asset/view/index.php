284a97a4e265eb2ca0e76366c7d55eb1bdc92c2d
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-04-28 19:32:32 -0500

    update

--- asset/view/index.php
+++ asset/view/index.php
@@ -205,7 +205,7 @@ if(isLoggedIn() == false) {
 
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item d-flex align-items-start">
-                                    <div class="ms-2 w-25">
+                                    <div class="ms-2" style="width: 15%;">
                                         <div class="fw-bold">Asset Tag Number</div>
                                     </div>
                                     <span class=""><?php echo $off_asset_tag_no; ?></span>
