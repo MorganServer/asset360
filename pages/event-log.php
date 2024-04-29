777ef810a360ca03505f640abe479c484c93eb5a
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-04-29 11:08:35 -0500

    ASSET-1 1.17 - finishing up styles

--- pages/event-log.php
+++ pages/event-log.php
@@ -45,7 +45,7 @@ if(isLoggedIn() == false) {
             .btn-group.dropend .dropdown-toggle::after {
                 display: none;
             }
-            .btn-group.dropend .dropdown-toggle:focus {
+            .btn:focus {
                 outline: none;
             }
         </style>
