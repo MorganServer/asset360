d71244a4eced3f7ac2868fd9eac1231ba5b5912f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-01 17:46:45 -0500

    ASSET-4 1.14 - adding functionality to mulitple pages

--- pages/my_account.php
+++ pages/my_account.php
@@ -43,9 +43,9 @@ if(isLoggedIn() == false) {
 
             <div class="row">
                 <div class="col">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card">
                       <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
+                        <h5 class="card-title">Account Information</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                         <a href="#" class="card-link">Card link</a>
@@ -54,7 +54,7 @@ if(isLoggedIn() == false) {
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card">
                       <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
