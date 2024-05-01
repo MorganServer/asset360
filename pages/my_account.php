a3679db8636262e7de582e56c0da5d6f30c1c0ad
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-01 18:46:44 -0500

    ASSET-4 1.23 - adding functionality to mulitple pages

--- pages/my_account.php
+++ pages/my_account.php
@@ -54,7 +54,7 @@ if(isLoggedIn() == false) {
                     </div>
                 </div>
                 <div class="col">
-                    <div class="card">
+                    <div class="card w-75">
                       <div class="card-body">
                         <h5 class="card-title">Card title</h5>
                         <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
