bbe5a0bafb44a2a4b477d2e683fcf30189fd5e6c
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-01 08:29:06 -0500

    ASSET-3 1.28 - building jira tickets tab

--- api/get_jira_data.php
+++ api/get_jira_data.php
@@ -1,9 +1,4 @@
 <?php
-
-
-
-
-
 // Assuming $off_asset_tag_no contains the current asset tag
 $assetTag = $_GET['asset_tag']; // Assuming the asset tag is passed as a query parameter
 
@@ -23,41 +18,6 @@ if ($response === false) {
     echo json_encode(["error" => "Failed to fetch data from Jira API", "url" => "Endpoint: " . $url]);
 } else {
     // Return the response data to the client
-    
-    $response = getJiraIssue($url);
     echo $response;
-}
-
-
-
-// Function to create a Jira issue
-function getJiraIssue($url) {
-    // $jiraApiUrl = 'https://garrett-morgan.atlassian.net/rest/api/3/issue';
-
-
-    $jiraUsername = "garrett.morgan.pro@gmail.com";
-    $one = "ATATT3xFfGF0rALQ3ASzKULCbilrrrykWqEfW8yJlCjhGCHW0mBSQcSaGP";
-    $two = "Ewxq8DC39D1ElsXBo7Wp3tHueO26Jp3AZ2IQNmfrq5urdZ91wfhGWB5xWd";
-    $three = "gTqWD8qGQ7qbt-CcgAp4WWeSlO0WrN30VB238osKbafBEBHz1WPbUSWeyh";
-    $four = "dXhAHA2kA=46A5779A";
-    $jiraApiToken = $one . $two . $three . $four;
-
-    $contextOptions = array(
-        'http' => array(
-            'method' => 'GET',
-            'header' => "Content-Type: application/json\r\n" .
-                        "Authorization: Basic " . base64_encode($jiraUsername . ':' . $jiraApiToken) . "\r\n",
-            // 'content' => $issueDataJson,
-            'ignore_errors' => true 
-        )
-    );
-
-
-    $context = stream_context_create($contextOptions);
-
-
-    $response = file_get_contents($url, false, $context);
-
-    return $response;
 }
 ?>
