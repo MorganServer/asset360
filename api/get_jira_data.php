111a4df8bbe0e33d015ff50b4b7824a0f07acf49
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-01 08:16:16 -0500

    ASSET-3 1.18 - building jira tickets tab

--- api/get_jira_data.php
+++ api/get_jira_data.php
@@ -9,16 +9,13 @@ $fields = "summary"; // Define the fields you want to retrieve
 // Construct the URL for the Jira API endpoint
 $url = "https://garrett-morgan.atlassian.net/rest/api/3/search?jql=" . $jqlQuery . "&fields=" . $fields;
 
-// Display the endpoint being used
-echo "<p>Endpoint: " . $url . "</p>";
-
 // Make a request to the Jira API endpoint
 $response = file_get_contents($url);
 
 // Check if the request was successful
 if ($response === false) {
     // Handle the error
-    echo "<p>Failed to fetch data from Jira API</p>";
+    echo json_encode(["error" => "Failed to fetch data from Jira API"]);
 } else {
     // Return the response data to the client
     echo $response;
