96136854e168cf0f5bf7746e5d40234d2b324eed
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-01 10:01:33 -0500

    ASSET-2 1.61 - adding jira issue create button

--- asset/view/index.php
+++ asset/view/index.php
@@ -76,7 +76,7 @@ if(isLoggedIn() == false) {
             .scrollable-table-container {
                 overflow-y: auto; /* Enable vertical scrollbar */
                 border: 1px solid black;
-                height: calc(100vh - 20px);
+                height: calc(100vh - 300px);
             }
             .sticky-header th {
               position: sticky;
