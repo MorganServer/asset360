ffcf4e5ab1e6d7cd0b2b0cae2896266252d24334
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-01 11:11:24 -0500

    ASSET-2 1.74 - adding jira issue create button

--- api/get_jira_all_data.php
+++ api/get_jira_all_data.php
@@ -30,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     // $assetTag = $_GET['asset_tag'];
 
     // Construct the JQL query string dynamically
-    $jqlQuery = "project in (SG, INFRA)";
+    $jqlQuery = "project in (SG, INFRA) AND issueType in (10029,10030)";
     $fields = "summary, issuetype"; // Define the fields you want to retrieve
 
     // Construct the URL for the Jira API endpoint
