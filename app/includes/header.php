c69114c6a72175f40f615dc8b867bdd895cf4ac5
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-02 17:56:07 -0500

    ASSET-5 - updating  multiple pages - 1.49

--- app/includes/header.php
+++ app/includes/header.php
@@ -5,45 +5,60 @@
     </a>
     <div class="header_right">
       <div class="dropdown d-flex notify_dropdown">
-        <i class="bi bi-bell-fill" id="notificationDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-        <div class="dropdown-menu" aria-labelledby="notificationDropdown" id="notificationMenu" style="width: 500px; padding: 15px; border-radius: 10px !important;">
-            <ul class="list-group list-group-flush">
-                <!-- Notifications will be dynamically added here -->
-                <?php
-                    $notify_sql = "SELECT * FROM notifications WHERE acknowledged = 0 ORDER BY notification_created DESC LIMIT 5";
-                    $notify_result = mysqli_query($conn, $notify_sql);
-                    if($notify_result) {
-                        $notify_num_rows = mysqli_num_rows($notify_result);
-                        if($notify_num_rows > 0) {
-                            while ($notify_row = mysqli_fetch_assoc($notify_result)) {
-                                $created = $notify_row['notification_created'];
-                                $notify_created = !empty($created) ? date_format(date_create($created), 'M d, Y') : '-';
-                ?>
-                <li class="list-group-item d-flex justify-content-between align-items-center">
-                  <div class="d-flex flex-column">
-                    <?php echo $notify_row['details']; ?>
-                    <span class="text-secondary" style="font-size: 12px;"><?php echo $notify_created; ?></span>
-                  </div>
-                    
-                    <form action="" method="POST">
-                        <!-- Hidden input field to send notification ID -->
-                        <input type="hidden" name="notification_id" value="<?php echo $notify_row['notification_id']; ?>">
-                        <!-- Acknowledge button -->
-                        <button type="submit" name="acknowledge" class="btn btn-primary btn-sm">Acknowledge</button>
-                    </form>
-                </li>
-                <?php
-                            }
-                        } else {
-                            echo '<li class="list-group-item">No new notifications</li>';
-                        }
-                    } else {
-                        echo '<li class="list-group-item">Error fetching notifications</li>';
-                    }
-                ?>
-            </ul>
-        </div>
+          <i class="bi bi-bell-fill" id="notificationDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <!-- Display badge with notification count here -->
+              <?php
+                  // Count the number of notifications that are not acknowledged
+                  $unread_notification_sql = "SELECT COUNT(*) AS unread_count FROM notifications WHERE acknowledged = 0";
+                  $unread_notification_result = mysqli_query($conn, $unread_notification_sql);
+                  if ($unread_notification_result && mysqli_num_rows($unread_notification_result) > 0) {
+                      $unread_count_row = mysqli_fetch_assoc($unread_notification_result);
+                      $unread_count = $unread_count_row['unread_count'];
+                      // Display badge only if there are unread notifications
+                      if ($unread_count > 0) {
+                          echo '<span class="badge badge-pill badge-danger">' . $unread_count . '</span>';
+                      }
+                  }
+              ?>
+          </i>
+          <div class="dropdown-menu" aria-labelledby="notificationDropdown" id="notificationMenu" style="width: 500px; padding: 15px; border-radius: 10px !important;">
+              <ul class="list-group list-group-flush">
+                  <!-- Notifications will be dynamically added here -->
+                  <?php
+                      $notify_sql = "SELECT * FROM notifications WHERE acknowledged = 0 ORDER BY notification_created DESC LIMIT 5";
+                      $notify_result = mysqli_query($conn, $notify_sql);
+                      if($notify_result) {
+                          $notify_num_rows = mysqli_num_rows($notify_result);
+                          if($notify_num_rows > 0) {
+                              while ($notify_row = mysqli_fetch_assoc($notify_result)) {
+                                  $created = $notify_row['notification_created'];
+                                  $notify_created = !empty($created) ? date_format(date_create($created), 'M d, Y') : '-';
+                  ?>
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                      <div class="d-flex flex-column">
+                          <?php echo $notify_row['details']; ?>
+                          <span class="text-secondary" style="font-size: 12px;"><?php echo $notify_created; ?></span>
+                      </div>
+                      <form action="" method="POST">
+                          <!-- Hidden input field to send notification ID -->
+                          <input type="hidden" name="notification_id" value="<?php echo $notify_row['notification_id']; ?>">
+                          <!-- Acknowledge button -->
+                          <button type="submit" name="acknowledge" class="btn btn-primary btn-sm">Acknowledge</button>
+                      </form>
+                  </li>
+                  <?php
+                              }
+                          } else {
+                              echo '<li class="list-group-item">No new notifications</li>';
+                          }
+                      } else {
+                          echo '<li class="list-group-item">Error fetching notifications</li>';
+                      }
+                  ?>
+              </ul>
+          </div>
       </div>
+
       <div class="dropdown d-flex dropdown">
         <i class="bi bi-plus-circle-fill" id="profileDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
         <div class="dropdown-menu" aria-labelledby="profileDropdown">
