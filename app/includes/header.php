e520e1fc8924087f2afd9e46354d364238341580
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-02 17:58:52 -0500

    ASSET-5 - updating  multiple pages - 1.51

--- app/includes/header.php
+++ app/includes/header.php
@@ -18,7 +18,7 @@
                       $unread_count = $unread_count_row['unread_count'];
                       // Display badge only if there are unread notifications
                       if ($unread_count > 0) {
-                          echo '<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">' . $unread_count . '</span>';
+                          echo '<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="font-size: 14px;">' . $unread_count . '</span>';
                       }
                   }
               ?>
