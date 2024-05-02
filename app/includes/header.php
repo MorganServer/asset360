d14c8984cac5a19d957cad63e377d519bcfde9e1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-02 17:12:01 -0500

    ASSET-5 - updating  multiple pages - 1.31

--- app/includes/header.php
+++ app/includes/header.php
@@ -6,7 +6,7 @@
     <div class="header_right">
       <div class="dropdown d-flex">
         <i class="bi bi-bell-fill" id="notificationDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></i>
-        <div class="dropdown-menu" aria-labelledby="notificationDropdown" id="notificationMenu" style="width: 300px; padding: 15px; border-radius: 10px !important;">
+        <div class="dropdown-menu" aria-labelledby="notificationDropdown" id="notificationMenu" style="width: 500px; padding: 15px; border-radius: 10px !important;">
           <ul class="list-group list-group-flush">
             <!-- Notifications will be dynamically added here -->
             <?php
