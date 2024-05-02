83c87447f75d4164417656c0e0161288379dd2b4
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-01 19:12:43 -0500

    ASSET-4 1.37 - adding functionality to mulitple pages

--- pages/my_account.php
+++ pages/my_account.php
@@ -48,7 +48,7 @@ if(isLoggedIn() == false) {
                         <h5 class="card-title">Account Information</h5>
                         <p class="card-text">
                             <div class="text-secondary d-flex mx-auto" style="border-radius: 100px; border: 2.5px solid #6c757d; width: 100px; height: 100px">
-                                <i style="font-size: 60px;" class="bi bi-person-fill text-center"></i>
+                                <img src="../assets/images/profile-pic.png" alt="">
                             </div>
 
                         </p>
