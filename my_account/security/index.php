9c20c1321aa06a5f21b676ad5dbe8b70e9bb9629
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 16:05:19 -0500

    ASSET-6 - updating - 1.88

--- my_account/security/index.php
+++ my_account/security/index.php
@@ -34,110 +34,55 @@ if(isLoggedIn() == false) {
     <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
     <?php include(ROOT_PATH . "/app/includes/sidebar.php"); ?>
 
-    <!-- main-container -->
-        <div class="container" style="padding: 0 75px 0 75px;">
-            <h2 class="mt-4">
-                Change Password
-            </h2>
-            <hr>
+    <!-- Main container -->
+    <div class="container" style="padding: 0 75px 0 75px;">
+        <h2 class="mt-4">Change Password</h2>
+        <hr>
 
-            <div class="mb-4" style="background-color: rgb(229, 233, 236); border-radius: 5px; padding: 10px;">
-                <p>
-                    You may change your password at any time by following the steps below:
-                </p>
-                <ul>
-                    <li>Enter your current password and new password. You will need to enter the new password twice in order to confirm that you have entered it correctly.</li>
-                    <li>An email with a confirmation number will be sent to your email address on record. Enter the confirmation number on the next page.</li>
-                    <li>The password change will be effective immediately.</li>
-                </ul>
-            </div>
-            
+        <!-- Information -->
+        <div class="mb-4" style="background-color: rgb(229, 233, 236); border-radius: 5px; padding: 10px;">
+            <!-- Steps -->
+            <p>You may change your password at any time by following the steps below:</p>
+            <ul>
+                <li>Enter your current password and new password. You will need to enter the new password twice in order to confirm that you have entered it correctly.</li>
+                <li>An email with a confirmation number will be sent to your email address on record. Enter the confirmation number on the next page.</li>
+                <li>The password change will be effective immediately.</li>
+            </ul>
+        </div>
 
-                <div class="col">
-                        <?php
-                            $user_id = $_SESSION['user_id'];
-
-                            $a_sql = "SELECT * FROM users WHERE user_id = $user_id";
-                            $a_result = mysqli_query($conn, $a_sql);
-                            if($a_result) {
-                                $a_num_rows = mysqli_num_rows($a_result);
-                                if($a_num_rows > 0) {
-                                    while ($a_row = mysqli_fetch_assoc($a_result)) {
-                                        $id                     = $a_row['user_id'];
-                                        $fname                  = $a_row['fname'];
-                                        $lname                  = $a_row['lname'];
-                                        $uname                  = $a_row['uname'];
-                                        $email                  = $a_row['email'];
-                                        $account_type           = $a_row['account_type'];
-                                        $account_created        = $a_row['account_created'];
-
-                                        // Format maintenance schedule if not null
-                                        $f_account_created = !empty($account_created) ? date_format(date_create($account_created), 'M d, Y') : '-';
-                                    
-                                    }}}
-                        ?>
-                    <div class="card w-100" style="border-top: 4px solid rgb(0, 43, 73); border-radius: 3px !important;">
-                      <div class="card-body p-4">
-                        <h5 class="card-title">Account Information</h5>
-                        <p class="card-text">
+        <!-- Account Information -->
+        <div class="col">
+            <div class="card w-100" style="border-top: 4px solid rgb(0, 43, 73); border-radius: 3px !important;">
+                <div class="card-body p-4">
+                    <h5 class="card-title">Account Information</h5>
+                    <p class="card-text">
+                        <!-- Profile Picture -->
                         <div class="text-secondary d-flex justify-content-center align-items-center mx-auto" style="border-radius: 100%; border: 4px solid #6c757d; width: 150px; height: 150px; overflow: hidden;">
                             <img src="../assets/images/bg-profile-pic.JPG" style="width: 93%; height: 93%; border-radius: 100%;" alt="">
-                            
                         </div>
+                        <!-- Account Type and Creation Date -->
                         <div class="text-center mt-3">
-                            <p class="text-secondary text-capitalize">
-                                <?php echo $account_type; ?> Account
-                            </p>
-                            <p class="text-secondary" style="margin-top: -10px;">
-                                Member since <?php echo $f_account_created; ?>
-                            </p>
+                            <p class="text-secondary text-capitalize"><?php echo $account_type; ?> Account</p>
+                            <p class="text-secondary" style="margin-top: -10px;">Member since <?php echo $f_account_created; ?></p>
                         </div>
                         <br>
-                        
-
+                        <!-- Account Update Form -->
                         <form>
-                          <div class="row mb-3">
-                            <label for="fname" class="col-sm-2 col-form-label">First Name</label>
-                            <div class="col-sm-10">
-                              <input type="text" class="form-control" id="fname" name="fname" value="<?php echo $fname; ?>">
+                            <div class="row mb-3">
+                                <label for="fname" class="col-sm-2 col-form-label">First Name</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="fname" name="fname" value="<?php echo $fname; ?>">
+                                </div>
                             </div>
-                          </div>
-                          <div class="row mb-3">
-                            <label for="lname" class="col-sm-2 col-form-label">Last Name</label>
-                            <div class="col-sm-10">
-                              <input type="text" class="form-control" id="lname" name="lname" value="<?php echo $lname; ?>">
-                            </div>
-                          </div>
-                          <div class="row mb-3">
-                            <label for="uname" class="col-sm-2 col-form-label">User Name</label>
-                            <div class="col-sm-10">
-                              <input type="text" class="form-control" id="uname" name="uname" value="<?php echo $uname; ?>">
-                            </div>
-                          </div>
-                          <div class="row mb-3">
-                            <label for="email" class="col-sm-2 col-form-label">Email</label>
-                            <div class="col-sm-10">
-                              <input type="text" class="form-control" id="email" name="email" value="<?php echo $email; ?>">
-                            </div>
-                          </div>
-                          <!-- <div class="row mb-3">
-                            <label for="password" class="col-sm-2 col-form-label">Password</label>
-                            <div class="col-sm-10">
-                              <input type="password" class="form-control" id="password" name="password">
-                            </div>
-                          </div> -->
-                    
-                          
-                          <button type="submit" name="update" class="btn btn-primary">Update</button>
+                            <!-- Other form fields -->
+                            <!-- Update Button -->
+                            <button type="submit" name="update" class="btn btn-primary">Update</button>
                         </form>
-
-                        </p>
-                    </div>
+                    </p>
                 </div>
             </div>
-            
         </div>
+    </div>
     <!-- END main-container -->
-
 </body>
 </html>
