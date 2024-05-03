802b610136edaffcf9d32c8d95a97dc6d6d43481
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 13:16:00 -0500

    ASSET-6 - updating - 1.41

--- pages/mobile.php
+++ pages/mobile.php
@@ -37,7 +37,7 @@ if(isLoggedIn() == false) {
     <!-- main-container -->
     <div class="container" style="padding: 0 75px 0 75px;">
             <h2 class="mt-4">
-                Servers
+                Mobile Devices
             </h2>
             <hr>
 
