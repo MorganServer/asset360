b31116a0efccd7b3da153169d0fdc200f5c00155
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 12:57:04 -0500

    ASSET-6 - updating - 1.39

--- pages/ip-address.php
+++ pages/ip-address.php
@@ -102,7 +102,19 @@ if(isLoggedIn() == false) {
                     <td><?php echo $f_maintenance_schedule ? $f_maintenance_schedule : '-'; ?></td>
                     <td><?php echo $f_audit_schedule ? $f_audit_schedule : '-'; ?></td>
                     <td><?php echo $status ? $status : '-'; ?></td>
-                    <td style="font-size: 20px;"><a href="view-app.php?viewid=<?php echo $id; ?>" class="view"><i class="bi bi-eye text-success"></i></a> &nbsp; <a href="update-app.php?updateid=<?php echo $id; ?>"><i class="bi bi-pencil-square" style="color:#005382;"></a></i> &nbsp; <a href="ip-address.php?deleteip=<?php echo $id; ?>" class="delete"><i class="bi bi-trash" style="color:#941515;"></i></a></td>
+                    <td style="font-size: 20px;">
+                        <a href="<?php echo BASE_URL; ?>/asset/view/?id=<?php echo $id; ?>" class="view">
+                            <i class="bi bi-eye text-success"></i>
+                        </a> 
+                        &nbsp; 
+                        <a href="<?php echo BASE_URL; ?>/asset/update/?id=<?php echo $id; ?>">
+                            <i class="bi bi-pencil-square" style="color:#005382;"></i>
+                        </a> 
+                        &nbsp; 
+                        <a href="<?php echo BASE_URL; ?>/asset/delete/?id=<?php echo $id; ?>" class="delete">
+                            <i class="bi bi-trash" style="color:#941515;"></i>
+                        </a>
+                    </td>
                 </tr>
                 <?php
                         }
