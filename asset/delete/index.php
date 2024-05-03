60e2c9d4c32cd7ece7e29485c3e7bdc5122101b3
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 12:31:10 -0500

    ASSET-6 - updating - 1.27

--- asset/delete/index.php
+++ asset/delete/index.php
@@ -1,4 +1,9 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+
 // Include the necessary files
 include_once '../../path.php'; // Assuming this defines BASE_URL
 include_once BASE_URL . '/app/database/connection.php';
