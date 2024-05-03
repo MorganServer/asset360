688393e1cf38f047553a9eaefa6a5892e92a84e1
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 07:51:37 -0500

    update

--- pages/audit-schedule.php
+++ pages/audit-schedule.php
@@ -91,7 +91,7 @@ if(isLoggedIn() == false) {
                         <a class="badge text-bg-primary text-decoration-none me-2" style="font-size: 14px; cursor: pointer;" data-bs-toggle="modal" data-bs-target="#scheduleModal">
                             <div class="position-relative">
                                 <i class="bi bi-calendar-fill"></i>
-                                <span class="position-absolute bottom-0 translate-middle" style="font-size: 10px; margin-bottom: -5%; margin-right: 70%;"><i class="bi bi-clock-history"></i></span>  &nbsp;Reschedule Audit
+                                <span class="position-absolute bottom-0 translate-middle" style="font-size: 10px; margin-bottom: -5%; margin-right: 60%;"><i class="bi bi-clock-history"></i></span>  &nbsp;Reschedule Audit
                             </div>
                         </a>
                     </td>                    
