19919a865e90f7fe8049c1ac2dd6a502120e5faa
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 12:16:10 -0500

    ASSET-6 - updating - 1.19

--- app/functions/delete-asset.php
+++ app/functions/delete-asset.php
@@ -1,7 +1,7 @@
 <?php
 
-if(isset($_GET['deleteid'])) {
-    $id = $_GET['deleteid'];
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
 
     $sql = "DELETE FROM assets WHERE asset_id=$id";
     $result = mysqli_query($conn, $sql);
