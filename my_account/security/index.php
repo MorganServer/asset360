304b413e239d1db28262cacf90943e31fa42eeb9
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 16:47:12 -0500

    ASSET-6 - updating - 2.15

--- my_account/security/index.php
+++ my_account/security/index.php
@@ -108,7 +108,7 @@ if(isLoggedIn() == false) {
                             <div class="row mb-3">
                                 <label for="password" class="col-sm-2 col-form-label">New Password</label>
                                 <div class="col-sm-10">
-                                  <div class="input-group">
+                                  <div class="input-group d-flex">
                                     <input
                                       type="password"
                                       class="form-control rounded mt-1 w-50"
