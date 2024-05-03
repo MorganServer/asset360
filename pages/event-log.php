392b5e14dae4de2aca25ec04377a86bc27c4f03f
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-03 05:17:13 -0500

    update

--- pages/event-log.php
+++ pages/event-log.php
@@ -93,8 +93,9 @@ if(isLoggedIn() == false) {
                                     <tr>
                                       <th scope="col">Issue Key</th>
                                       <th scope="col">Asset Tag No</th>
-                                      <th scope="col">Issue Type</th>
                                       <th scope="col">Summary</th>
+                                      <th scope="col">Issue Type</th>
+                                      <th scope="col">Status</th>
                                       <th scope="col">Created</th>
                                       <th scope="col">Updated</th>
                                       <th scope="col">Link</th>
@@ -123,7 +124,7 @@ if(isLoggedIn() == false) {
                                             document.getElementById("jiraTableBody").innerHTML = "";
                                             data.issues.forEach(issue => {
                                                 var newRow = document.createElement("tr");
-                                                newRow.innerHTML = `<td>${issue.key}</td><td><a href="https://asset360.morganserver.com/asset/view/?id=${issue.fields.labels[0]}" target="_blank" class="" style="">${issue.fields.labels[1]}</a></td><td>${issue.fields.summary}</td><td>${issue.fields.issuetype.name}</td><td>${new Date(issue.fields.created).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}</td><td>${new Date(issue.fields.updated).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}</td><td><a href="https://garrett-morgan.atlassian.net/browse/${issue.key}" target="_blank" class="badge text-bg-primary text-decoration-none" style="font-size: 14px;">Visit</a></td>`;
+                                                newRow.innerHTML = `<td>${issue.key}</td><td><a href="https://asset360.morganserver.com/asset/view/?id=${issue.fields.labels[0]}" target="_blank" class="" style="">${issue.fields.labels[1]}</a></td><td>${issue.fields.summary}</td><td>${issue.fields.issuetype.name}</td><td>${issue.fields.status.name}</td><td>${new Date(issue.fields.created).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}</td><td>${new Date(issue.fields.updated).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' })}</td><td><a href="https://garrett-morgan.atlassian.net/browse/${issue.key}" target="_blank" class="badge text-bg-primary text-decoration-none" style="font-size: 14px;">Visit</a></td>`;
                                                 document.getElementById("jiraTableBody").appendChild(newRow);
                                             });
                                         } else {
