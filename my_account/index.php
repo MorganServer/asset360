d6d7c7a4feab7258ac5eb3b608af197493fe664e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-04 10:27:06 -0500

    ASSET-6 - updating

--- my_account/index.php
+++ my_account/index.php
@@ -154,7 +154,7 @@ if(isLoggedIn() == false) {
                     </div>
                 </div>
                 <div class="col-sm-4">
-                    <div class="card mb-3 w-10" style="border-top: 4px solid gray; border-radius: 3px !important;">
+                    <div class="card mb-3 w-100" style="border-top: 4px solid gray; border-radius: 3px !important;">
                         <div class="card-body">
                             <p class="card-text text-center" style="font-size: 50px;"><i class="bi bi-person-fill-lock"></i></p>
                             <h5 class="card-title text-center">User Access</h5>
