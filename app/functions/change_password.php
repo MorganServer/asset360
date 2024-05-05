f6678ab9c54ebb4604a9251f2614389b3fe9389e
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-05 15:28:49 -0500

    ASSET-7 adding functionality

--- app/functions/change_password.php
+++ app/functions/change_password.php
@@ -52,7 +52,7 @@ if(isset($_POST['change'])) {
         }
 
         // Redirect to the next page
-        header("Location: " . BASE_URL . "/my_account/security/email_code_change.php");
+        // header("Location: " . BASE_URL . "/my_account/security/email_code_change.php");
         exit(); // Ensure script execution stops after redirecting
     }
 }
