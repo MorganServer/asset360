3e472ec47eafa995254bac362bde298b4fc643ea
Garrett Morgan <garrett.morgan.pro@gmail.com>
2024-05-06 15:18:52 -0500

    ASSET-7 adding functionality

--- asset/view/index.php
+++ asset/view/index.php
@@ -172,7 +172,7 @@ if(isLoggedIn() == false) {
                                     <input type="hidden" id="asset_tag" name="asset_tag" value="<?php echo '[' . $off_asset_tag_no. '] '; ?>">
                                     <input type="hidden"  id="actual_asset_tag" name="actual_asset_tag" value="<?php echo $off_asset_tag_no; ?>">
                                     <input type="hidden" id="asset_id" name="asset_id" value="<?php echo $off_id; ?>">
-                                    <button type="button" class="badge text-bg-primary text-decoration-none me-2" style="font-size: 14px; cursor: pointer;">
+                                    <button type="submit" class="badge text-bg-primary text-decoration-none me-2" style="font-size: 14px; cursor: pointer;">
                                         <i class="bi bi-shield-fill-check"></i> &nbsp;Perform Audit
                                     </button>
                                 </form>
@@ -663,7 +663,7 @@ if(isLoggedIn() == false) {
 
 <!-- audit script -->
     <script>
-        document.getElementById('auditModalForm').addEventListener('submit', function(event) {
+        document.getElementById('auditForm').addEventListener('submit', function(event) {
         event.preventDefault(); // Prevent form submission
         var actual_asset_tag = document.getElementById('actual_asset_tag').value;
         var asset_id = document.getElementById('asset_id').value;
@@ -717,8 +717,8 @@ if(isLoggedIn() == false) {
             // Handle response
             console.log(data);
             // Close modal
-            var myModal = new bootstrap.Modal(document.getElementById('auditModal'));
-            myModal.hide();
+            // var myModal = new bootstrap.Modal(document.getElementById('auditModal'));
+            // myModal.hide();
         })
         .catch(error => {
             console.error('Error:', error);
